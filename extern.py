6199db6bff53ba9ea9a58ab6dd88605e6549de1c
xiefusheng <43473100+xiefusheng@users.noreply.github.com>
2018-12-02 22:13:03 +0800

    开始界面以及帮助文档

--- extern.py
+++ extern.py
@@ -5,5 +5,11 @@
 screen='主显示窗口'
 cursor='光标图像'
 start_background='开始背景图'
-start_button='开始按钮'
+start_button='单人模式'
+start_button_online='联机对战'
+start_button_setting='设置'
+start_button_help='帮助'
+start_button_custom='自定义'
+help_text='帮助文档'
+help_text_state=0 #帮助文档状态
 game_state='游戏状态'
--- test.py
+++ test.py
@@ -27,10 +27,29 @@ while True:
     for event in pygame.event.get():
         if event.type==QUIT:
             exit()
-
     if(extern.game_state==GAMEINIT):
 #   开始画面贴图
         start.start_blit()
+#   光标绘制
+        start.cursorshow()
+#   鼠标动作响应
+        start.mouseclick_respond(event)
+    if(extern.game_state==GAMEINIT1):
+#   开始画面贴图
+        start.start_blit()
+#   光标绘制
+        start.cursorshow()
+#   鼠标动作响应
+        start.mouseclick_respond(event)
+    elif(extern.game_state==GAMEHELP):
+        start.start_blit()
+#   光标绘制
+        start.cursorshow()
+#   鼠标动作响应
+        start.mouseclick_respond(event)
+    elif(extern.game_state==GAMEHELP2):
+        start.start_blit()
+        start.help_blit()
 #   光标绘制
         start.cursorshow()
 #   鼠标动作响应
