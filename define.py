106f8f84da234b253fd518f9c8a0fbab1b77f2e6
KFDeng <44015769+KFDeng@users.noreply.github.com>
2018-12-15 22:50:16 +0800

    加入了技能相关的宏定义
    加入了技能相关的宏定义

--- define.py
+++ define.py
@@ -1,103 +1,127 @@
 #
-#   宏定义模块，存储一些常量
+#   �궨��ģ�飬�洢һЩ����
 #
 
-#   主窗口尺寸
+#   �����ڳߴ�
 mainwindow_size=(1440,900)
-#   光标尺寸
+#   ���ߴ�
 cursor_size=(30,40)
-#   开始按钮尺寸
+#   ��ʼ��ť�ߴ�
 start_button_size=(250,50)
-#单人模式选择界面缩略图尺寸
+#����ģʽѡ���������ͼ�ߴ�
 single_choose_b_size=(500,300)
 single_choose_bc_size=(550,340)
 single_choose_p_size=(200,200)
 single_choose_pc_size=(230,230)
 
-#   帮助文档
+#   �����ĵ�
 help_text_size=(800,600)
 
-#   游戏状态常量
-#   初始状态
+#   �Զ���ģʽ����ͼ��С
+custom_thumbnail_size=(100,160)
+custom_pic_choose_size=(700,700)
+
+single_game_portrait_size=(80,80)
+single_game_skill_size=(40,40)
+single_game_hp_size=(100,20)
+single_game_map_size=(300,300)
+single_game_p_size=(15,15)
+
+#   ��Ϸ״̬����
+#   ��ʼ״̬
 GAMEINIT=0
-#   游戏开始状态
+#   ��Ϸ��ʼ״̬
 GAMESTART=1
-#   游戏载入状态
+#   ��Ϸ����״̬
 GAMELOAD=-1
-#   帮助所需状态
+#   ��������״̬
 GAMEHELP=2
 GAMEHELP2=3
 GAMEINIT1=4
-#   单人模式选择状态
+#   ����ģʽѡ��״̬
 GAMESINGLECHOOSE=5
+#   ����
+GAMESETTING=10
+#   �Զ���ģʽѡ��״̬
+GAMECUSTOMCHOOSE=20
+#   �Զ�������ģʽ
+GAMECUSTOMC=21
+#   �Զ���ؿ�ģʽ
+GAMECUSTOMG=22
 
-#   碰撞阈值
+#   ��ײ��ֵ
 COLLISIONTHRESHOLD=10
 
-#   帧率
+#   ֡��
 fps=25
 
-#   主游戏最底图层路径
+#   ����Ϸ���ͼ��·��
 gameinterface_filename='Resource/interface/gameinterface.png'
 
-#   人物状态
-#   静止状态
+#   ����״̬
+#   ��ֹ״̬
 PLAYERSTATIC=-1
-#   移动状态
+#   �ƶ�״̬
 PLAYERMOVE=0
-#   攻击状态
+#   ����״̬
 PLAYERATTACK=-2
-#   被攻击状态
+#   ������״̬
 PLAYERATTACKED=-3
-#   技能一状态
+#   ����һ״̬
 PLAYERSKILL1=-4
-#   技能二状态
+#   ���ܶ�״̬
 PLAYERSKILL2=-5
-#   技能三状态
+#   ������״̬
 PLAYERSKILL3=-6
-#   死亡状态
+#   ����״̬
 PLAYERDEAD=-7
 
-#   敌人状态
-#   静止状态
+#   ����״̬
+#   ��ֹ״̬
 ENEMYSTATIC=-1
-#   移动状态
+#   �ƶ�״̬
 ENEMYMOVE=0
-#   攻击状态
+#   ����״̬
 ENEMYATTACK=-2
-#   被攻击状态
+#   ������״̬
 ENEMYATTACKED=-3
-#   死亡状态
+#   ����״̬
 ENEMYDEAD=-4
 
 
-#上      
+#��      
 MOVEUP=0
-#下      
+#��      
 MOVEDOWN=1
-#左      
+#��      
 MOVELEFT=2
-#右      
+#��      
 MOVERIGHT=3
-#左上 
+#���� 
 MOVEUPLEFT=4
-#右上 
+#���� 
 MOVEUPRIGHT=5
-#左下 
+#���� 
 MOVEDOWNLEFT=6
-#右下 
+#���� 
 MOVEDOWNRIGHT=7
-#1技能
+#1����
 SKILL1=8
-#2技能
+#2����
 SKILL2=9
-#3技能
+#3����
 SKILL3=10
-#被攻击信号
+#�������ź�
 ATTACKED=11
-#死亡信号
+#�����ź�
 DIE=12
 
-#   移动序列
+#��������
+SKILLBALLSTRAIGHT=0
+SKILLBALLSINUS=1
+SKILLBALLCIRCLE=2
+SKILLRETURN=3
+
+#   �ƶ�����
 movex=[0,0,-1,1,-1,1,-1,1]
 movey=[-1,1,0,0,-1,-1,1,1]
