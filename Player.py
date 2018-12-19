33e32e1dbf7a1738f89742e1b070ec8928edd570
KFDeng <44015769+KFDeng@users.noreply.github.com>
2018-12-19 12:42:12 +0800

    重构，需添加加载资源
    重构，需添加加载资源

--- Player.py
+++ Player.py
@@ -2,90 +2,43 @@
 #   人物类
 #
 
-from define import *
-import extern
-import pygame
-from pygame.locals import *
-from math import pow
-import Item
+from Define import *
+import Extern as Et
+import Skill
+#import pygame
+#from pygame.locals import *
+
+class PlayerInfo():
+    def __init__(self):##############需要加载资源#######################
+        self.site = [200,400]
+        self.max_life = 0
+        self.life_value = 0
+        self.state = PLAYERSTATIC
+        self.pic_direction = RIGHT
+        self.size = [0,0]
+        self.count = 0
+        self.visible = True
 
 class Player():
-    def __init__(self):
-        self.site='人物位置(二维)'
-        self.max_life='人物最大生命值'
-        self.life_value='人物生命值'
-        self.power='攻击力'
-        self.attack_range='射程'
-        self.velocity='速度'
-        self.state=PLAYERSTATIC
-        self.movable=True
-        self.direction='人物朝向'
-        self.pic_direction='贴图朝向'
-        self.signal=None
-        self.size='人物的大小'
-        self.game='当前游戏指针'
-        self.count='小状态计数器'
-        self.skill1type='技能一类型'
-        self.skill1time='技能一上次发动的时间'
-        self.skill1_cd='技能一冷却时间'
-        self.skill2type='技能二类型'
-        self.skill2time='技能二上次发动的时间'
-        self.skill2_cd='技能二冷却时间'
-        self.skill3type='技能三类型'
-        self.skill3time='技能三上次发动的时间'
-        self.skill3_cd='技能三冷却时间'
-        self.skill_direction='技能方向'
-        self.freezetime='被攻击冻结时间'
-        self.return_x='回归位置技能记录位置'
-        self.return_y='回归位置技能记录位置'
-        self.return_released='是否释放了回归位置技能'
-        self.attacked_skill_list=[]
-        self.load()
-    
-    #贴图
-    def player_update_blit(self,n):
-        if self.direction==MOVEUPRIGHT or self.direction==MOVERIGHT or self.direction==MOVEDOWNRIGHT:
-            self.pic_direction=MOVERIGHT
-        elif self.direction==MOVEUPLEFT or self.direction==MOVELEFT or self.direction==MOVEDOWNLEFT:
-            self.pic_direction=MOVELEFT
-        picname=None
-        if n==0:
-            picname=extern.character_resource.pic_static1
-        elif n==1:
-            picname=extern.character_resource.pic_static2
-        elif n==2:
-            picname=extern.character_resource.pic_static3
-        elif n==3:
-            picname=extern.character_resource.pic_move1
-        elif n==4:
-            picname=extern.character_resource.pic_move2
-        elif n==5:
-            picname=extern.character_resource.pic_move3
-        elif n==6:
-            picname=extern.character_resource.pic_attack1 
-        elif n==7:
-            picname=extern.character_resource.pic_attack2
-        elif n==8:
-            picname=extern.character_resource.pic_attack3
-        elif n==9:
-            picname=extern.character_resource.pic_attacked1
-        elif n==10:
-            picname=extern.character_resource.pic_attacked2
-        elif n==11:
-            picname=extern.character_resource.pic_attacked3
-        elif n==12:
-            picname=extern.character_resource.pic_dead1
-        if self.pic_direction==MOVELEFT:
-            extern.singleplayergame_resource.pic_temp.blit(picname[1],
-            (int(self.site[0]-self.size[0]/2),int(self.site[1]-self.size[1]/2)))
-        else :
-            extern.singleplayergame_resource.pic_temp.blit(picname[0],
-            (int(self.site[0]-self.size[0]/2),int(self.site[1]-self.size[1]/2)))
-
+    def __init__(self,pinfo):##############需要加载资源#######################
+        self.info = pinfo
+        self.velocity = [0, 0]
+        self.movable = [True,True,True,True]    #上下左右方向是否可移动
+        self.game = None    #游戏指针
+        self.skill_time = [0,0,0]   #技能123的上次发动时间
+        self.skill_cd = [0,0,0] #技能123的冷却时间
+        self.freeze_time = 0    #被攻击后僵直时间
+        self.signal = None
+        self.direction = MOVERIGHT
+        self.skill_type = [0,0,0]
+        self.max_count = 12
+        self.return_site = [0,0]
+        self.return_released = False
+        self.skill_list = [[],[],[]]
 
 # 状态更新,有限状态机,每个不同角色单独实现
     def update(self):
-        if self.state==PLAYERSTATIC:
+        if self.info.state==PLAYERSTATIC:
             if self.signal == None:
                 pass
             elif ((self.signal<8) & (self.signal>-1)):
@@ -94,162 +47,137 @@ class Player():
             elif self.signal==ATTACKED:
                 self.switchState(PLAYERATTACKED)
             elif self.signal==SKILL1:
-                if ((extern.last_fresh_time-self.skill1time)>self.skill1_cd):
+                if ((Et.fresh_time-self.skill_time[0])>self.skill_cd[0]):
                     self.switchState(PLAYERSKILL1)
             elif self.signal==SKILL2:
-                if ((extern.last_fresh_time-self.skill1time)>self.skill2_cd):
+                if ((Et.fresh_time-self.skill_time[1])>self.skill_cd[1]):
                     self.switchState(PLAYERSKILL2)
             elif self.signal==SKILL3:
-                if ((extern.last_fresh_time-self.skill1time)>self.skill3_cd):
+                if ((Et.fresh_time-self.skill_time[2])>self.skill_cd[2]):
                     self.switchState(PLAYERSKILL3)
             elif self.signal==DIE:
                 self.switchState(PLAYERDEAD)
-            self.count=(self.count+1)%12
-            self.showPic(12,0,1,2)
-        elif self.state==PLAYERMOVE:
+            self.info.count=(self.info.count+1)%self.max_count
+        elif self.info.state==PLAYERMOVE:
             if self.signal==None:
                 self.switchState(PLAYERSTATIC)
             elif ((self.signal<8) & (self.signal>-1)):
                 self.direction=self.signal
-                self.count=(self.count+1) % 12
+                self.info.count=(self.info.count+1)%self.max_count
                 self.move()
             elif self.signal==ATTACKED:
                 self.switchState(PLAYERATTACKED)
             elif self.signal==SKILL1:
-                if ((extern.last_fresh_time-self.skill1time)>self.skill1_cd):
+                if ((Et.fresh_time-self.skill_time[0])>self.skill_cd[0]):
                     self.switchState(PLAYERSKILL1)
             elif self.signal==SKILL2:
-                if ((extern.last_fresh_time-self.skill2time)>self.skill2_cd):
+                if ((Et.fresh_time-self.skill_time[1])>self.skill_cd[1]):
                     self.switchState(PLAYERSKILL2)
             elif self.signal==SKILL3:
-                if ((extern.last_fresh_time-self.skill3time)>self.skill3_cd):
+                if ((Et.fresh_time-self.skill_time[2])>self.skill_cd[2]):
                     self.switchState(PLAYERSKILL3)
             elif self.signal==DIE:
                 self.switchState(PLAYERDEAD)
-            self.showPic(12,3,4,5)
-        elif self.state==PLAYERSKILL1:
+        elif self.info.state==PLAYERSKILL1:
             self.skillstate(1)
-        elif self.state==PLAYERSKILL2:
+        elif self.info.state==PLAYERSKILL2:
             self.skillstate(2)
-        elif self.state==PLAYERSKILL3:
+        elif self.info.state==PLAYERSKILL3:
             self.skillstate(3)
-        elif self.state==PLAYERATTACKED:
-            if self.count==self.freezetime:
+        elif self.info.state==PLAYERATTACKED:
+            if self.info.count==self.freeze_time:
                 self.switchState(PLAYERSTATIC)
             else:
-                self.count=self.count+1
+                self.info.count=self.info.count+1
             if self.signal==ATTACKED:
                 self.switchState(PLAYERATTACKED)
             elif self.signal==DIE:
                 self.switchState(PLAYERDEAD)
-            self.showPic(self.freezetime,9,10,11)
-        elif self.state==PLAYERDEAD:
-            self.player_update_blit(12)
+        elif self.info.state==PLAYERDEAD:
+            pass
 
     #单独处理正在发起技能的状态
     def skillstate(self,n):
-        if self.count==6:
-            if n==1:
-                self.actSkill(self.skill1type)
-                self.skill1time=extern.last_fresh_time-10/fps
-            elif n==2:
-                self.actSkill(self.skill2type)
-                self.skill2time=extern.last_fresh_time-10/fps
-            elif n==3:
-                self.actSkill(self.skill3type)
-                self.skill3time=extern.last_fresh_time-10/fps
-        elif self.count==12:
+        if self.info.count==int(self.max_count/2):
+            self.actSkill(n)
+            self.skill_time[n-1]=Et.fresh_time-10/fps
+        elif self.info.count==self.max_count:
             self.switchState(PLAYERSTATIC)
         if self.signal==ATTACKED:
             self.switchState(PLAYERATTACKED)
         elif self.signal==DIE:
             self.switchState(PLAYERDEAD)
-        self.count=self.count+1
-        self.showPic(12,6,7,8)
+        self.info.count=self.info.count+1
 
     #状态转换，计数置零
     def switchState(self,state):
-        self.count=0
-        self.state=state
-
-    #根据当前count显示player_update_blit中编号为n1,n2,n3的三帧图
-    def showPic(self,maxcount,n1,n2,n3):
-        if self.count<maxcount/3:
-            self.player_update_blit(n1)
-        elif self.count<maxcount*2/3:
-            self.player_update_blit(n2)
-        else:
-            self.player_update_blit(n3)
+        self.info.count=0
+        self.info.state=state
 
     #发起不同的技能，新技能在此添加
-    def actSkill(self,skilltype):
-        if skilltype==SKILLBALLSTRAIGHT:
-            self.releaseBall(1)
-        elif skilltype==SKILLBALLSINUS:
-            self.releaseBall(2)
-        elif skilltype==SKILLBALLCIRCLE:
-            self.releaseBall(3)
-        elif skilltype==SKILLRETURN:
+    def actSkill(self,n):
+        skill_type=self.skill_type[n-1]
+        if skill_type==SKILLBALLSTRAIGHT or skill_type==SKILLBALLSINUS or skill_type==SKILLBALLCIRCLE:
+            new_skill=self.releaseBall(skill_type)
+        elif skill_type==SKILLRETURN:
             if self.return_released==True:
-                self.site[0]=self.return_x
-                self.site[1]=self.return_y
+                self.info.site=self.return_site[:]
             else:
-                self.return_x=self.site[0]
-                self.return_y=self.site[1]
+                self.return_site=self.info.site[:]
             self.return_released=not self.return_released
+            new_skill=None
+        self.skill_list[n-1].append(new_skill)
 
     #专门用于扔出实体球的技能，n选择球轨迹
-    def releaseBall(self,n):
-        tempskill=Item.Skill()
-        tempskill.game=self.game
-        if n==1:
-            tempskill.resource=extern.skill_resource
-        elif n==2:
-            tempskill.resource=extern.skill_resource2
-        elif n==3:
-            tempskill.resource=extern.skill_resource3
-        tempskill.initsite=self.site[:]
-        tempskill.inittime=extern.last_fresh_time
-        tempskill.caster=self
-        tempskill.direction=self.skill_direction
-        tempskill.site=self.site[:]
-        tempskill.size=tempskill.resource.size
-        tempskill.damage=tempskill.resource.damage
-        self.game.skill_list.append(tempskill)
+    def releaseBall(self,skill_type):
+        if skill_type==SKILLBALLSTRAIGHT:
+            new_skill=Skill.SkillBallStraight()
+            #new_skill.resource=Et.skill_resource##############需要加载资源#######################
+        elif skill_type==SKILLBALLSINUS:
+            new_skill=Skill.SkillBallSinus()
+            #new_skill.resource=Et.skill_resource2##############需要加载资源#######################
+        elif skill_type==SKILLBALLCIRCLE:
+            new_skill=Skill.SkillBallCircle()
+            #new_skill.resource=Et.skill_resource3##############需要加载资源#######################
+        new_skill.game=self.game
+        new_skill.init_site=self.info.site[:]
+        new_skill.init_time=Et.fresh_time
+        new_skill.caster=self
+        new_skill.direction=self.direction
+        new_skill.info.site=self.info.site[:]
+        #new_skill.info.size=tempskill.resource.size##############需要加载资源#######################
+        #new_skill.damage=new_skill.resource.damage##############需要加载资源#######################
+        self.game.skill_list.append(new_skill)
+        return new_skill
 
 # 事实上的构造函数,init里面只是声明一下变量,之所以不赋值,是因为这些值
 # 可能要从文件里读取或者是根据游戏设置而定
+        '''
     def load(self):
-        self.size=extern.character_resource.size
-        self.count=0
-        self.velocity=extern.character_resource.velocity
+        self.size=Et.character_resource.size
+        self.info.count=0
+        self.velocity=Et.character_resource.velocity
         self.movex=[self.velocity[0]*x for x in movex]
         self.movey=[self.velocity[1]*y for y in movey]
         self.direction=MOVERIGHT
-        self.freezetime=extern.character_resource.freezetime
+        self.freezetime=Et.character_resource.freezetime
         self.return_released=False
         ###以下暂时使用，请改入初始化###
         self.skill1type=SKILLRETURN
         self.skill2type=SKILLBALLSINUS
         self.skill3type=SKILLBALLCIRCLE
         ###以上暂时使用，请改入初始化###
+        '''
 
-    def move(self):
+    def move(self):##############需要加载资源#######################
         if self.movable:
-            self.site[0]=self.site[0]+self.movex[self.signal]
-            if self.site[0]>(extern.singleplayergame_resource.size[0]-int(self.size[0]/2)):
-                self.site[0]=extern.singleplayergame_resource.size[0]-int(self.size[0]/2)
-            if self.site[0]<int(self.size[0]/2):
-                self.site[0]=int(self.size[0]/2)
-            self.site[1]=self.site[1]+self.movey[self.signal]
-            if self.site[1]>(extern.singleplayergame_resource.size[1]-int(self.size[1]/2)):
-                self.site[1]=extern.singleplayergame_resource.size[1]-int(self.size[1]/2)
-            if self.site[1]<int(self.size[1]/2):
-                self.site[1]=int(self.size[1]/2)
-
-
-# 键盘侠类,继承自Player类
-class KeyBoardMan(Player):
-    def __init__(self):
-        super(KeyBoardMan,self).__init__()
-        pass
+            self.info.site[0]=self.info.site[0]+self.movex[self.signal]
+            if self.info.site[0]>(Et.singleplayergame_resource.size[0]-int(self.size[0]/2)):
+                self.info.site[0]=Et.singleplayergame_resource.size[0]-int(self.size[0]/2)
+            if self.info.site[0]<int(self.size[0]/2):
+                self.info.site[0]=int(self.size[0]/2)
+            self.info.site[1]=self.info.site[1]+self.movey[self.signal]
+            if self.info.site[1]>(Et.singleplayergame_resource.size[1]-int(self.size[1]/2)):
+                self.info.site[1]=Et.singleplayergame_resource.size[1]-int(self.size[1]/2)
+            if self.info.site[1]<int(self.size[1]/2):
+                self.info.site[1]=int(self.size[1]/2)
---
+++ Skill.py
@@ -0,0 +1,111 @@
+#
+#   技能类文件
+#
+
+#   会调用update(),修改技能状态
+#   调用influence(),对单位造成影响
+#   应有忽略单位列表ignore_list
+#   delflag
+
+from Define import *
+import Extern as Et
+import Player
+import Game
+import Item
+import math
+
+class SkillInfo():
+    def __init__(self):##############需要加载资源#######################
+        self.site=[0,0]
+        self.size=Et.skill_resource.size
+        self.visible=True
+
+class Skill():
+    def __init__(self,sinfo):##############需要加载资源#######################
+        self.info=sinfo
+        self.direction=MOVERIGHT
+        self.game=None
+        self.init_time=0
+        self.duration=Et.skill_resource.duration
+        self.delflag=False
+        self.velocity=Et.skill_resource.velocity
+        self.movex=[]
+        self.movey=[]
+        self.caster=None
+        self.damage=Et.skill_resource.damage
+        self.ignore_list=[]
+        self.ignore_player=False
+        self.init_site=[0,0]
+        self.resource=None
+        
+    def collisionJudge(self,collider):
+        if ((abs(collider.info.site[0]-self.info.site[0])-(collider.info.size[0]+self.info.size[0])/2<0)&
+            (abs(collider.info.site[1]-self.info.site[1])-(collider.info.size[1]+self.info.size[1])/2<0)):
+            return True
+        else:
+            return False
+        
+    def defaultInfluence(self):
+        if self.caster==self.game.player:
+            for enemy in self.game.enemy_list:
+                if self.collisionJudge(enemy):
+                    if not enemy in self.ignore_list:
+                        enemy.info.life_value-=self.damage
+                        self.ignore_list.append(enemy)
+                else:
+                    if enemy in self.ignore_list:
+                        self.ignore_list.remove(enemy)
+        else:
+            if self.collisionJudge(self.game.player):
+                self.game.player.info.life_value-=self.damage
+                self.ignore_player=True
+            else:
+                self.ignore_player=False
+        
+    def load(self):
+        self.movex=[self.velocity*x for x in movex]
+        self.movey=[self.velocity*y for y in movey]
+        
+class SkillBallStraight(Skill):
+    def update(self):
+        if Et.fresh_time-self.init_time>self.duration:
+            self.delflag=True
+        else:
+            self.info.site[0]=self.site[0]+self.movex[self.direction]
+            self.info.site[1]=self.site[1]+self.movey[self.direction]
+            
+    def influence(self):
+        self.defaultInfluence()
+        
+class SkillBallSinus(Skill):
+    def update(self):
+        if Et.fresh_time-self.init_time>self.duration:
+            self.delflag=True
+        else:
+            sinmovex=10*self.velocity*(Et.fresh_time-self.init_time)
+            sinmovey=50*math.sin(2*math.pi*(Et.fresh_time-self.init_time))
+            transmat=[
+                [0,1,-1,0],[0,-1,1,0],[-1,0,0,-1],[1,0,0,1],
+                [-1,1,-1,-1],[1,1,-1,1],[-1,-1,1,-1],[1,-1,1,1]
+            ]
+            self.info.site=[
+                self.init_site[0]+int(sinmovex*transmat[self.direction][0]+sinmovey*transmat[self.direction][1]),
+                self.init_site[1]+int(sinmovex*transmat[self.direction][2]+sinmovey*transmat[self.direction][3])
+            ]
+                
+    def influence(self):
+        self.defaultInfluence()
+        
+class SkillBallCircle(Skill):
+    def update(self):
+        if Et.fresh_time-self.init_time>self.duration:
+            self.delflag=True
+        else:
+            dx=1.5*max(self.caster.info.size)*math.cos(2*math.pi*(Et.fresh_time-self.init_time))
+            dy=1.5*max(self.caster.info.size)*math.sin(2*math.pi*(Et.fresh_time-self.init_time))
+            self.info.site=[
+                self.caster.info.site[0]+int(dx),
+                self.caster.info.site[1]+int(dy)]
+                
+    def influence(self):
+        self.defaultInfluence()
